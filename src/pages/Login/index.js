a44ca78c8f315b37fdb06c8c926a4e5f992648c6
lucasGabrielDeAA <lg.technus@gmail.com>
2020-03-26 12:17:54 -0300

    logins's behavior added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
+import { toast } from 'react-toastify';
 
 import logo from "../../assets/logo.svg";
 import heroesImage from "../../assets/heroes.png";
 
+import api from '../../services/api';
+
 import "./styles.css";
 
 export default function Login() {
+  const history = useHistory();
+  const [token, setToken] = useState('');
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { token });
+
+      await localStorage.setItem('auth_token', response.data.token);
+      await localStorage.setItem('ong_name', response.data.name);
+      history.push('/profile');
+    } catch (error) {
+      toast.error('Erro ao se autenticar, tente novamente');
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logo} alt="Logo" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu login</h1>
 
-          <input placeholder="Seu Token" />
+          <input placeholder="Seu Token" value={token} onChange={e => setToken(e.target.value)} />
           <button className="button" type="submit">
             Entrar
           </button>
